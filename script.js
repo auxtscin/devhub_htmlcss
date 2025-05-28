f96d3650bf39a30d99d8b6446333aff2bef2547d
cindy <cindyaustin011206@gmail.com>
2025-05-28 12:49:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// function addNumbers() {
+//     let a = prompt("What is yout first number?");
+//     let b = prompt("What is your second number?");
+//     const result = parseInt(a) + parseInt(b);
+//     alert("Your number is " + result);  
+// }
+
+// const addNumbers = function() {
+//     let a = prompt("What is yout first number?");
+//     let b = prompt("What is your second number?");
+//     const result = parseInt(a) + parseInt(b);
+//     alert("Your number is " + result);  
+// }
+
+const addNumbers = () => {
+    // let a = prompt("What is yout first number?");
+    // let b = prompt("What is your second number?");
+
+    const firstNum = document.querySelector("#firstNum").value;
+    const secondNum = document.querySelector("#secondNum").value;
+    const result = parseInt(firstNum) + parseInt(secondNum);
+
+    // alert("Your number is " + result);  
+
+    // const newDiv = document.createElement("div");
+    // const buttonContainer = document.querySelector("#buttonContainer");
+    // newDiv.innerHTML = "Your result is " + result;
+    // buttonContainer.appendChild(newDiv); 
+
+    const resultDiv = document.querySelector("#result");
+    resultDiv.innerHTML = "Your result is " + result;
+};
+
+const changeBackgroundButton = document.querySelector("#changeBackgroundButton");
+
+// function changeBackgroundColor() {
+//     document.body.style.background = "purple";
+// }
+// changeBackgroundButton.addEventListener('click', changeBackgroundColor);
+
+changeBackgroundButton.addEventListener('click', () => {
+    document.body.style.background = "purple";
+});
